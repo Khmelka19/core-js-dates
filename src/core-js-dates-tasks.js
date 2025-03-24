e5ae015e68a7af20633962a8ecc4d51627c904e7
Oxana Savianok <oxanas8833@gmail.com>
2025-03-24 17:33:24 +0300

    feat: add a solution to getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -221,8 +221,14 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const curDay = new Date(date.getTime());
+
+  while (curDay.getDay() !== 5 || curDay.getDate() !== 13) {
+    curDay.setDate(curDay.getDate() + 1);
+  }
+
+  return curDay;
 }
 
 /**
